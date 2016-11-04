61d0f612c2803ce52c3dec6d4e5f082d7ba4d8c9
IntelliFinder <snirhordan@gmail.com>
2016-11-04 15:10:04 +0200

    Update Percolation.java

--- precolation/Percolation.java
+++ precolation/Percolation.java
@@ -1,9 +1,19 @@
 public class Percolation {
-   public Percolation(int n)                // create n-by-n grid, with all sites blocked
+   public Percolation(int n){
+       
+      //n*n grid
+      //create two nodes extra as first and last row -so rowcount-2=colcount=n
+
+   }                // create n-by-n grid, with all sites blocked
    public void open(int row, int col)       // open site (row, col) if it is not open already
    public boolean isOpen(int row, int col)  // is site (row, col) open?
    public boolean isFull(int row, int col)  // is site (row, col) full?
-   public boolean percolates()              // does the system percolate? This implemetns the union operations discussed previously in course
-
-   public static void main(String[] args)   // test client (optional)
+    /* does the system percolate? 
+    Create an alias for the bottom and top rows so connected to single site
+   This implement the union operations discussed previously in course*/
+   public boolean percolates(Percolation p) {
+   //define top node as tree root
+   //check if top node has path to bottom nose
+   }
+   public static void main(String[] args){}   // test client (optional)
 }
